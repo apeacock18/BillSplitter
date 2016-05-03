7a6ca884939d6092d19004ea70080f1b41a55562
davismariotti <mariottidavis.bchs@gmail.com>
2016-05-02 18:11:11 -0700

    Remove unnecessary functions

--- BillSplitter/GroupTableViewController.swift
+++ BillSplitter/GroupTableViewController.swift
@@ -9,26 +9,8 @@
 import UIKit
 
 class GroupTableViewController: UITableViewController {
-    
-    
+
     var groups: Array<Group> = []
-    
-    func loadSampleTransactions() {
-        let photo1 = UIImage(named: "dog1")!
-        let t1 = ["name":"Corgi Butt", "photo":photo1, "amount": 42.00]
-
-        let photo2 = UIImage(named: "dog2")!
-        let t2 = ["name":"Doug Barkman", "photo":photo2, "amount": 47.00]
-
-        let photo3 = UIImage(named: "dog3")!
-        let t3 = ["name":"Snowball", "photo":photo3, "amount": 0.00]
-        
-        let photo4 = UIImage(named: "default")!
-        let t4 = ["name":"Billy", "photo":photo4, "amount": 876.01]
-        
-        //transactions += [t1, t2, t3, t4]
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,25 +38,6 @@ class GroupTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return groups.count
     }
-    
-    /*override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cellIdentifier = "MemberCell"
-        
-        tableView.registerNib(UINib(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! MemberCell
-        
-        let t = transactions[indexPath.row]
-        
-        cell.name.text = t["name"] as? String
-        cell.avatar.image = t["photo"] as? UIImage
-        let amount: Double = t["amount"] as! Double
-        cell.amount.text = String(format:"%.2f", amount)
-        if(amount != 0.0) {
-            cell.backgroundColor = UIColor.redColor()
-        }
-        
-        return cell
-    }*/
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         if groups.count != 0 {
